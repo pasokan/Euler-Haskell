fa87805f3ff9f0c40e0588f3f8a923dc96cae921
Asokan Pichai <pasokan@talentsprint.com>
2016-01-07 16:46:18 +0530

    Simple implementation. isPalindrome may need to be moved to Euler

---
+++ Euler-004.hs
@@ -0,0 +1,14 @@
+-- A palindromic number reads the same both ways. The largest palindrome
+-- made from the product of two 2-digit numbers is 9009 = 91 × 99.
+
+-- Find the largest palindrome made from the product of two 3-digit numbers.
+
+isPalindrome :: Integer -> Bool
+isPalindrome n = n == read (reverse $ show n)
+
+palindromes3x3 = [a * b | a <- [100..999], b <- [a..999], isPalindrome (a * b)]
+
+main = do
+    putStrLn $ show $ maximum palindromes3x3
+
+
