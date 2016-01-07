0dce2aad0d9301532555f6fa6bf8cad32cb02b9d
Asokan Pichai <pasokan@talentsprint.com>
2016-01-07 13:11:10 +0530

    Completed the clean up from Euler-003

--- Euler.hs
+++ Euler.hs
@@ -6,6 +6,13 @@ module Euler
 ( num2Digits
 , digits2Num
 , fibs
+, maxFactor
+, isPrime
+, primeFactors
+, factors
+, primes
+
+
 ) where
 
 import Data.Char
@@ -25,9 +32,14 @@ maxFactor :: Integer -> Integer
 maxFactor n = fst $ last $ zip [1..] $ takeWhile (<= n) [f * f | f <- [1..]]
 
 isPrime :: Integer -> Bool
-isPrime n = and $ [ rem n f /= 0 | f <- [3,5 .. (maxFactor n)]]
+isPrime n = if rem n 2 == 0 then False else and $ [ rem n f /= 0 | f <- [3,5 .. (maxFactor n)]]
 
-coprimeTo30 = [1, 7, 11, 13, 17, 19, 23, 29]
-primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29] ++ [ n + r | n <- [30, 60 ..], r <- coprimeTo30, isPrime (n+r)]  
+factors :: Integer -> [Integer]
+factors nn = concat  $ [[x, div nn x] | x <- [2.. maxFactor nn], rem nn x == 0]
 
-factors nn = [x | x <- [2.. maxFactor nn], rem nn x == 0] 
+primeFactors :: Integer -> [Integer]
+primeFactors nn = [ f | f <- factors nn, isPrime f]
+
+primes = primesBelow30 ++ [ n + r | n <- [30, 60 ..], r <- coprimeTo30, isPrime (n+r)]  
+    where primesBelow30 = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29]
+          coprimeTo30 = [1, 7, 11, 13, 17, 19, 23, 29]
