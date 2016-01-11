b38bd8eb93d56df134015b8543ce326d8aa6c752
Asokan Pichai <pasokan@talentsprint.com>
2016-01-11 20:28:02 +0530

    Refactored primes related functions to a Primes.hs module

--- Euler.hs
+++ Euler.hs
@@ -30,16 +30,3 @@ fibs = 0 : 1 : zipWith (+) fibs (tail fibs)
 
 maxFactor :: Integer -> Integer
 maxFactor n = fst $ last $ zip [1..] $ takeWhile (<= n) [f * f | f <- [1..]]
-
-isPrime :: Integer -> Bool
-isPrime n = if rem n 2 == 0 then False else and $ [ rem n f /= 0 | f <- [3,5 .. (maxFactor n)]]
-
-factors :: Integer -> [Integer]
-factors nn = concat  $ [[x, div nn x] | x <- [2.. maxFactor nn], rem nn x == 0]
-
-primeFactors :: Integer -> [Integer]
-primeFactors nn = [ f | f <- factors nn, isPrime f]
-
-primes = primesBelow30 ++ [ n + r | n <- [30, 60 ..], r <- coprimeTo30, isPrime (n+r)]  
-    where primesBelow30 = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29]
-          coprimeTo30 = [1, 7, 11, 13, 17, 19, 23, 29]
